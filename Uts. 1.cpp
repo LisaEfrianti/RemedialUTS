386336675e84b15e71a842817fa6a258b6a089cb
LisaEfrianti <49120337+LisaEfrianti@users.noreply.github.com>
2019-04-01 20:05:01 -0700

    Create Uts. 1.cpp

---
+++ Uts. 1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout<<"Nama : Lisa Efrianti"<<endl;
+  cout<<"Nim : 1829142035"<<endl;
+  cout<<"Kelas : Tekom B"<<endl;
+  cout<<"Tahun Angkatan : 2018"<<endl;
+  
+  
+  return 0;
+  }
